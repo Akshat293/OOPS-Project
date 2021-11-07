acf0eca9cf1facd0646af5793ca4bbd7f29e1cf9
Akshat Saxena <76419575+tom635@users.noreply.github.com>
2021-11-07 20:21:24 +0530

    Added Header files

--- OOPS_project.cpp
+++ OOPS_project.cpp
@@ -1,1 +1,38 @@
 // Hi GUYS i hope we will make this project number one
+//Online Shopping System
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <stack> 
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <queue>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const int mod=1e9+7;
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define  vec vector<ll>
+#define  Sort(a) sort(a.begin(),a.end())
+
+
+
+int main(){
+
+
+    
+    return 0;
+}
